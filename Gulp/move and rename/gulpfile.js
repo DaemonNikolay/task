b8f80c39b1e60baa3b184513f571ab08a51bdc22
DaemonNikolay <nikolay-eckert@mail.ru>
2017-11-27 09:31:25 +0700

    Перемещение с переименованием

---
+++ Gulp/move and rename/gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+
+gulp.task('move-and-rename', function() {
+    return gulp.src('css/*.+(css)')
+        .pipe(rename({ prefix: 'min_' }))
+        .pipe(gulp.dest('path/to'))
+});
